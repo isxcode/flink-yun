f3e5ba8252beccdc941e9558fe2cfb898dd1b42e
ispong <ispong@outlook.com>
2024-06-11 19:05:45 +0800

    格式化代码

--- flink-yun-dist/zhiliuyun/bin/start.sh
+++ flink-yun-dist/zhiliuyun/bin/start.sh
@@ -5,6 +5,9 @@ BASE_PATH=$(cd "$(dirname "$0")" || exit ; pwd)
 cd "${BASE_PATH}" || exit
 cd ".." || exit
 
+# 导入用户指定环境变量
+source "conf/zhiliuyun-env.sh"
+
 # 项目已启动不执行
 if [ -e "zhiliuyun.pid" ]; then
   pid=$(cat "zhiliuyun.pid")
@@ -20,8 +23,13 @@ if [ ! -f logs/flink-yun.log ]; then
   touch logs/flink-yun.log
 fi
 
-# 启动项目
-nohup java -jar -Xmx2048m lib/zhiliuyun.jar --spring.profiles.active=local --spring.config.additional-location=conf/ > /dev/null 2>&1 &
+# 运行至流云程序
+if ! command -v java &>/dev/null; then
+  nohup $JAVA_HOME/bin/java -jar -Xmx2048m lib/zhiliuyun.jar --spring.profiles.active=local --spring.config.additional-location=conf/ > /dev/null 2>&1 &
+else
+  nohup java -jar -Xmx2048m lib/zhiliuyun.jar --spring.profiles.active=local --spring.config.additional-location=conf/ > /dev/null 2>&1 &
+fi
 echo $! >zhiliuyun.pid
+
 echo "【至流云】: RUNNING"
 tail -f logs/flink-yun.log
