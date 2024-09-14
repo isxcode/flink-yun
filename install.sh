de3964d93608a4fdf10d2ca117efabe2b3cdec65
ispong <ispong@outlook.com>
2024-09-14 17:44:52 +0800

    优化install.sh脚本

--- install.sh
+++ install.sh
@@ -74,6 +74,7 @@ if [ ! -d "${CDC_DIR}" ]; then
     mkdir -p "${CDC_DIR}"
 fi
 if [ ! -f "${CDC_DIR}"/flink-connector-jdbc-3.1.2-1.18.jar ]; then
+  echo "flink-connector-jdbc-3.1.2-1.18.jar开始下载"
   curl -ssL "${OSS_DOWNLOAD_URL}"/flink-connector-jdbc-3.1.2-1.18.jar -o ${CDC_DIR}/flink-connector-jdbc-3.1.2-1.18.jar
   echo "flink-connector-jdbc-3.1.2-1.18.jar下载成功"
 fi
