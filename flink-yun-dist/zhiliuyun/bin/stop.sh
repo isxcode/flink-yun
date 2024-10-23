feda29d72e6e15e43a82e1b20ae0a4e2d7816309
ispong <ispong@outlook.com>
2024-10-23 15:49:42 +0800

    GH-149 [Bug] action流水异常 (#151)
    * 官网添加一个字
    
    * 杀死进程改成 -9 防止无法杀死进程

--- flink-yun-dist/zhiliuyun/bin/stop.sh
+++ flink-yun-dist/zhiliuyun/bin/stop.sh
@@ -9,7 +9,7 @@ cd ".." || exit
 if [ -e "zhiliuyun.pid" ]; then
   pid=$(cat "zhiliuyun.pid")
   if ps -p $pid >/dev/null 2>&1; then
-   kill -15 ${pid}
+   kill -9 ${pid}
    echo "【至流云】: CLOSED"
    exit 0
   fi
